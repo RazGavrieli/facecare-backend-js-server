c5eb93f01faae6b69aa069dff379b97ab55e9dba
Raz <razgavrieli@gmail.com>
2023-01-02 17:56:16 +0200

    cleaning function works properly AN-25

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,38 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+const app = admin.initializeApp()
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.test = functions.region('us-central1').https.onRequest(async () => { // This line is used for HTTPS triggering for testing
 
-exports.helloWorld = functions.region('us-central1').database.ref().onWrite((snap, context) => {
-    console.log("functions triggered by datachange:", snap.value)
+// exports.scheduledFunction = functions.pubsub.schedule('every 5 minutes').onRun((context) => { // This line is used for production
+    try {
+    console.log("cleaning old dates")
+    const root = await admin.database().ref().get();
+    var afterData = root.val(); // data after the write
+
+    openAppointments = afterData['OpenAppointment']
+    newOpenAppointments = {}
+    var today = new Date();
+    for (const [key, value] of Object.entries(openAppointments)) {
+        const [day, month, year] = key.split('-');
+        const currDate = new Date(+year, +month - 1, +day);
+        if (currDate < today) {
+            delete afterData['OpenAppointment'][key]
+        }
+    }
+    
+    const deleteApp = () => app.delete().catch(() => null);
+
+    return app.database().ref("OpenAppointment").set(afterData['OpenAppointment']).then(res => {
+      // Deleting the app is necessary for preventing concurrency leaks
+      return deleteApp().then(() => res);
+    }).catch(err => {
+      return deleteApp().then(() => Promise.reject(err));
+    });
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+
 });
+
